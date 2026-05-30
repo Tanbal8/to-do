7125679a8a6579f21998b1651baddf4e3adcfbac
Tanbal8 <tanbal.derakhty@gmail.com>
2026-05-30 17:24:14 +0330

    Updates

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'todo-v1';
+const urlsToCache = ['/', '/index.html', '/static/js/main.chunk.js', '/static/js/0.chunk.js', '/static/css/main.chunk.css'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
